9d100571264bc5f047fbc33e5a4202a104842861
emersonbly <emersonbly10@gmail.com>
2022-09-06 14:13:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -21,28 +21,43 @@ function seasonSelectChanged() {
 
   // Update page to match selected season
   if (season === "spring") {
-    seasonH2.innerHTML = "spring";
-    seasonH2.style.color = "#0E94D1";
-    seasonImg.src = "img/spring.jpg";
-    seasonAudio.src = "songs/vivaldi-spring.mp3";
-    document.body.style.backgroundColor = "#0E94D1";
+    setSpring();
   } else if (season === "summer") {
-    seasonH2.innerHTML = "summer";
-    seasonH2.style.color = "#1BA848";
-    seasonImg.src = "img/summer.jpg";
-    seasonAudio.src = "songs/vivaldi-summer.mp3";
-    document.body.style.backgroundColor = "#1BA848";
+    setSummer();
   } else if (season === "autumn") {
-    seasonH2.innerHTML = "autumn";
-    seasonH2.style.color = "#FE6732";
-    seasonImg.src = "img/autumn.jpg";
-    seasonAudio.src = "songs/vivaldi-autumn.mp3";
-    document.body.style.backgroundColor = "#FE6732";
+    setAutumn();
   } else if (season === "winter") {
-    seasonH2.innerHTML = "winter";
-    seasonH2.style.color = "#1C64B9";
-    seasonImg.src = "img/winter.jpg";
-    seasonAudio.src = "songs/vivaldi-winter.mp3";
-    document.body.style.backgroundColor = "#1C64B9";
+    setWinter();
   }
+}
+
+// season functions
+
+function setSpring() {
+  seasonH2.innerHTML = "spring";
+  seasonH2.style.color = "#0E94D1";
+  seasonImg.src = "img/spring.jpg";
+  seasonAudio.src = "songs/vivaldi-spring.mp3";
+  document.body.style.backgroundColor = "#0E94D1";
+}
+function setSummer() {
+  seasonH2.innerHTML = "summer";
+  seasonH2.style.color = "#1BA848";
+  seasonImg.src = "img/summer.jpg";
+  seasonAudio.src = "songs/vivaldi-summer.mp3";
+  document.body.style.backgroundColor = "#1BA848";
+}
+function setAutumn() {
+  seasonH2.innerHTML = "autumn";
+  seasonH2.style.color = "#FE6732";
+  seasonImg.src = "img/autumn.jpg";
+  seasonAudio.src = "songs/vivaldi-autumn.mp3";
+  document.body.style.backgroundColor = "#FE6732";
+}
+function setWinter() {
+  seasonH2.innerHTML = "winter";
+  seasonH2.style.color = "#1C64B9";
+  seasonImg.src = "img/winter.jpg";
+  seasonAudio.src = "songs/vivaldi-winter.mp3";
+  document.body.style.backgroundColor = "#1C64B9";
 }
